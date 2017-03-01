d92f5a0e41ee61c6d35c38903de7e48b58b7b753
Maikel Maciel Rönnau <maikel.ronnau@gmail.com>
2017-02-28 23:31:29 -0300

    Updated script to show the percent of each score hit.

--- source/label_folder.py
+++ source/label_folder.py
@@ -7,13 +7,16 @@ image_extensions = ['.jpg', '.png', '.jpeg', '.bmp', '.gif', '.tif', '.tiff']
 
 # change this as you see fit
 image_path = sys.argv[1]
+calculate_percentage = False
+
+if len(sys.argv) > 2:
+    calculate_percentage = True
 
 # Read in the image_data
 # image_data = tf.gfile.FastGFile(image_path, 'rb').read()
 
 # Loads label file, strips off carriage return
-label_lines = [line.rstrip() for line 
-                   in tf.gfile.GFile("retrained_labels.txt")]
+label_lines = [line.rstrip() for line in tf.gfile.GFile("retrained_labels.txt")]
 
 predicitons_score = [0 for x in range(len(label_lines))]
 
@@ -24,28 +27,29 @@ with tf.gfile.FastGFile("retrained_graph.pb", 'rb') as f:
     _ = tf.import_graph_def(graph_def, name='')
 
 txt_output = open(os.path.split(image_path)[0] + "/evaluation.txt", "w")
+number_files = len(os.listdir(image_path))-1
 
 with tf.Session() as sess:
 
     # Feed the image_data as input to the graph and get first prediction
     softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
-    
+
     i = 0
 
     for file in natsorted(os.listdir(image_path), key=lambda y: y.lower()):
-        
+
         if os.path.splitext(file)[1] in image_extensions:
 
             image_data = tf.gfile.FastGFile(image_path + file, 'rb').read()
 
             predictions = sess.run(softmax_tensor, \
                     {'DecodeJpeg/contents:0': image_data})
-            
+
             # Sort to show labels of first prediction in order of confidence
             top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
-            
+
             i += 1
-            print ("(" + str(i) + "/" + str(len(os.listdir(image_path))-1) + ")  " + file)
+            print "(" + str(i) + "/" + str(number_files) + ")  " + file
             txt_output.write("\nTarget: " + file + "\n\n")
 
             for node_id in top_k:
@@ -55,16 +59,24 @@ with tf.Session() as sess:
                 txt_output.write('%s (score = %.5f)' % (human_string, score) + "\n")
 
             predicitons_score[top_k[0]] += 1
-    
+
     print "\nFinal results: \n"
     txt_output.write("\nFinal results: \n")
-    
-    for i in range(len(label_lines)):
-        print label_lines[i] + ": " + str(predicitons_score[i])
-        txt_output.write("\n" + label_lines[i] + ": " + str(predicitons_score[i]))
 
-    print "\nTotal images: " + str(len(os.listdir(image_path))-1)
-    txt_output.write("\n\nTotal images: " + str(len(os.listdir(image_path))-1))
+    if calculate_percentage:
+        for i in range(len(label_lines)):
+            print label_lines[i] + ": " + str(predicitons_score[i]) + "\t(%.5f%%)" % (float(predicitons_score[i] * 100) / float(number_files))
+            txt_output.write("\n" + label_lines[i] + ": %i" % predicitons_score[i])
+
+        print "\nTotal images: %i" % number_files
+        txt_output.write("\n\nTotal images: %i" % number_files)
+    else:
+        for i in range(len(label_lines)):
+            print label_lines[i] + ": %i" % predicitons_score[i]
+            txt_output.write("\n" + label_lines[i] + ": %i" % predicitons_score[i])
+
+        print "\nTotal images: %i" % number_files
+        txt_output.write("\n\nTotal images: %i" % number_files)
 
 txt_output.write("\n")
 txt_output.close()
