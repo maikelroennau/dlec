9e24ed072aa3258540e74a40dfbb3f559d2f773c
Maikel Maciel Rönnau <maikel.ronnau@gmail.com>
2017-07-28 22:44:07 -0300

    Updated network architecture. Added configurations to parameters.

--- train.py
+++ train.py
@@ -9,36 +9,43 @@ from tflearn.data_utils import shuffle, to_categorical
 
 
 def train():
-    train_dataset = 'datasets/dogs_vs_cats/train'
+    train_dataset = 'datasets/ck_dataset/train'
     dataset_name = train_dataset.split('/')[1]
     validation_dataset = None
 
     number_of_classes = len(dataset_loader.get_classes(train_dataset))
-    learning_rate = 0.005
 
     image_width = 64
     image_height = 64
 
-    images, labels = dataset_loader.load_dataset_images(train_dataset, image_height, image_width, dataset_name, colored=True, load_backup=True, export_dataset=True)
-    X, X_test, Y, Y_test = train_test_split(images, labels, test_size=0.1, random_state=42)
+    learning_rate = 0.005
+    test_size = 0.1
+    batch_size = 0.01
+    epochs = 5
+
+    images, labels = dataset_loader.load_dataset_images(train_dataset, image_width, image_height, dataset_name, colored=True, load_backup=True, export_dataset=True)
+    X, X_test, Y, Y_test = train_test_split(images, labels, test_size=test_size, random_state=42)
+
+    del images, labels
 
     Y = to_categorical(Y, number_of_classes)
     Y_test = to_categorical(Y_test, number_of_classes)
 
-    model = cnn.get_network_architecture(image_height, image_width, number_of_classes, learning_rate, colored=True)
+    model = cnn.get_network_architecture(image_width, image_height, number_of_classes, learning_rate, colored=True)
 
     model = tflearn.DNN(
         model,
         tensorboard_verbose=0,
         tensorboard_dir='train_logs/',
-        max_checkpoints=2
+        max_checkpoints=None,
+        best_checkpoint_path='best_checkpoint/'
     )
 
     model.fit(
         X, Y,
         validation_set=(X_test, Y_test),
-        batch_size=int((len(X)*0.01)),
-        n_epoch=15,
+        batch_size=int((len(X) * batch_size)),
+        n_epoch=epochs,
         run_id='001',
         show_metric=True
     )
@@ -48,8 +55,6 @@ def train():
 
     print('Saving trained model to {}'.format('model'))
     model.save('final_model/final_model.tflearn')
-
-    del images, labels, X, X_test, Y, Y_test
 
 
 if __name__ == '__main__':
