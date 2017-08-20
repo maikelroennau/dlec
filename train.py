86ccc8f3d0d18c5f22e889ec3cb131fe00f4d0f3
Maikel Maciel Rönnau <maikel.ronnau@gmail.com>
2017-08-20 11:50:52 -0300

    Added feature to handle early stop.

--- train.py
+++ train.py
@@ -19,20 +19,26 @@ def create_directories():
         except OSError:
             os.makedirs(directory)
 
+def get_batch_size(number_of_images, percentage):
+    if (number_of_images * percentage) > 200:
+        return 200
+    else:
+        return int(number_of_images * percentage)
+
 def train():
-    train_dataset = 'datasets/dogs_vs_cats/train'
+    train_dataset = 'datasets/custom'
     dataset_name = train_dataset.split('/')[1]
     validation_dataset = None
 
     number_of_classes = len(dataset_loader.get_classes(train_dataset))
 
-    image_width = 64
-    image_height = 64
+    image_width = 48
+    image_height = 48
 
-    learning_rate = 0.005
+    learning_rate = 0.001
     test_size = 0.1
     batch_size = 0.01
-    epochs = 5
+    epochs = 25
 
     images, labels = dataset_loader.load_dataset_images(train_dataset, image_width, image_height, dataset_name, load_backup=True, export_dataset=True)
     X, X_test, Y, Y_test = train_test_split(images, labels, test_size=test_size, random_state=42)
@@ -47,20 +53,23 @@ def train():
 
     model = tflearn.DNN(
         model,
-        tensorboard_verbose=0,
+        tensorboard_verbose=3,
         tensorboard_dir='train_logs/',
         max_checkpoints=None,
         best_checkpoint_path='best_checkpoint/'
     )
 
-    model.fit(
-        X, Y,
-        validation_set=(X_test, Y_test),
-        batch_size=int((len(X) * batch_size)),
-        n_epoch=epochs,
-        run_id=dataset_name,
-        show_metric=True
-    )
+    try:
+        model.fit(
+            X, Y,
+            validation_set=(X_test, Y_test),
+            batch_size=get_batch_size(len(X), batch_size),
+            n_epoch=epochs,
+            run_id=dataset_name,
+            show_metric=True
+        )
+    except KeyboardInterrupt:
+        print('Training interruped')
 
     print('Saving trained model')
     model.save('final_model/final_model.tflearn')
