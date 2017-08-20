ab8dda027b53b8b39f980be88c69ab802a4bf955
Maikel Maciel Rönnau <maikel.ronnau@gmail.com>
2017-08-20 12:23:35 -0300

    Updated CNN architecture and train script.

--- train.py
+++ train.py
@@ -19,21 +19,27 @@ def create_directories():
         except OSError:
             os.makedirs(directory)
 
+def get_model(model_path, model):
+    print('\nRestoring model')
+    model.load(model_path)
+    
+    return model
+
 def get_batch_size(number_of_images, percentage):
     if (number_of_images * percentage) > 200:
         return 200
     else:
         return int(number_of_images * percentage)
 
-def train():
+def train(resume_training=False):
     train_dataset = 'datasets/custom'
     dataset_name = train_dataset.split('/')[1]
     validation_dataset = None
 
     number_of_classes = len(dataset_loader.get_classes(train_dataset))
 
-    image_width = 48
-    image_height = 48
+    image_width = 64
+    image_height = 64
 
     learning_rate = 0.001
     test_size = 0.1
@@ -49,13 +55,17 @@ def train():
     Y_test = to_categorical(Y_test, number_of_classes)
 
     model = cnn.get_network_architecture(image_width, image_height, number_of_classes, learning_rate)
-    create_directories()
+
+    if (resume_training):
+        model = get_model('final_model/final_model.tflearn', model)
+    else:
+        create_directories()
 
     model = tflearn.DNN(
         model,
         tensorboard_verbose=3,
         tensorboard_dir='train_logs/',
-        max_checkpoints=None,
+        max_checkpoints=3,
         best_checkpoint_path='best_checkpoint/'
     )
 
